2c3c79a7f0f02c0872669a6dd4a1ccdea2ab3d0f
tajemouti <tajemouti@gmail.com>
2023-10-11 01:07:04 +0100

    Implement correct_name method in person class

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -20,12 +23,20 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
 
-person1 = Person.new(0o001, 15, 'Adham', parent_permission: false)
-person2 = Person.new(0o002, 17, 'Najlae')
-person3 = Person.new(0o003, 40)
+person1 = Person.new(15, 'Adham', parent_permission: false)
+person2 = Person.new(17, 'Najlae')
+person3 = Person.new(40)
 
 puts person1.can_use_services? # false
 puts person2.can_use_services? # true
 puts person3.can_use_services? # true
+
+puts person1.correct_name # Adham
+puts person2.correct_name # Najlae
+puts person3.correct_name # Unknown
