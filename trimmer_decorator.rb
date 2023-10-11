3f804840082b1ca1b33fc63ed6028f57594e0d5e
tajemouti <tajemouti@gmail.com>
2023-10-11 02:36:51 +0100

    Make required change

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,11 @@ require './base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    @nameable.correct_name.slice(0, 10)
+    original_name = @nameable.correct_name
+    if original_name.length > 10
+      original_name.slice(0, 10)
+    else
+      original_name
+    end
   end
 end
