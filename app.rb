3b9b78965702bf66c0fc875956346ac169b52b9a
tajemouti <tajemouti@gmail.com>
2023-10-13 01:49:00 +0100

    Add list_books method

--- app.rb
+++ app.rb
@@ -11,5 +11,11 @@ class App
     @books = []
     @rentals = []
   end
+
+  def list_books
+    @books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
 end
 
