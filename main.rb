b595050aeefe8e56941a0adb625104c5fdeae602
tajemouti <tajemouti@gmail.com>
2023-10-18 20:17:34 +0100

    Update app and main files

--- main.rb
+++ main.rb
@@ -1,15 +1,18 @@
 require './app'
 require './menu'
+require 'json'
 
 def main
   puts 'Welcome to School Library App!'
   app = App.new
+  app.load_data
 
   loop do
     display_menu
     choice = gets.chomp.to_i
 
     if choice == 7
+      app.save_data
       puts 'Thank you for using this app!'
       break
     end
