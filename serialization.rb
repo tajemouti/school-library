68056a38fc11812f18b253f0f9364adcc4af2ca0
tajemouti <130588108+tajemouti@users.noreply.github.com>
2023-10-18 22:15:15 +0100

    Update serialization.rb file

---
+++ serialization.rb
@@ -0,0 +1,12 @@
+require 'json'
+
+module Serializition
+  def serialize_to_json(data, filename)
+    File.open(filename, 'w') { |file| file.puts JSON.generate(data) }
+  end
+
+  def deserialize_from_json(filename)
+    return [] unless File.exist?(filename)
+
+    JSON.parse(File.read(filename))
+end
